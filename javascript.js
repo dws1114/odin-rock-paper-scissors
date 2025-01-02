a2e740fd7c246f65c62828dee83122ab06d4d325
dws1114 <161630399+dws1114@users.noreply.github.com>
2025-01-02 12:00:44 -0500

    Edit play round function to make more concise

--- javascript.js
+++ javascript.js
@@ -3,36 +3,61 @@ function playGame() {
     let humanScore = 0;
     let computerScore = 0;
 
+    // function playRound(humanChoice, computerChoice) {
+    //     if (humanChoice === computerChoice) {
+    //         return `It's a tie! Both chose ${humanChoice}.`;
+    //     }
+    //     else if (humanChoice === "rock" && computerChoice === "paper") {
+    //         computerScore++;
+    //         return "You lose... paper beats rock!";
+    //     }
+    //     else if (humanChoice === "rock" && computerChoice === "scissors") {
+    //         humanScore++;
+    //         return "You win! rock beats scissors!";
+    //     }
+    //     else if (humanChoice === "paper" && computerChoice === "scissors") {
+    //         computerScore++;
+    //         return "You lose... scissors beats paper!";
+    //     }
+    //     else if (humanChoice === "paper" && computerChoice === "rock") {
+    //         humanScore++;
+    //         return "You win! paper beats rock!";
+    //     }
+    //     else if (humanChoice === "scissors" && computerChoice == "rock") {
+    //         computerScore++;
+    //         return "You lose... rock beats scissors!";
+    //     }
+    //     else if (humanChoice === "scissors" && computerChoice == "paper") {
+    //         humanScore++;
+    //         return "You win! scissors beats rock!";
+    //     }
+    //     else {
+    //         return "You must choose rock, paper, or scissors...";
+    //     }
+    // }
+
     function playRound(humanChoice, computerChoice) {
         if (humanChoice === computerChoice) {
             return `It's a tie! Both chose ${humanChoice}.`;
         }
-        else if (humanChoice === "rock" && computerChoice === "paper") {
+        else if (
+            (humanChoice === "rock" && computerChoice === "paper") ||
+            (humanChoice === "paper" && computerChoice === "scissors") ||
+            (humanChoice === "scissors" && computerChoice === "rock")
+        ) {
             computerScore++;
-            return "You lose... paper beats rock!";
+            return `You lose... ${computerChoice} beats ${humanChoice}`;
         }
-        else if (humanChoice === "rock" && computerChoice === "scissors") {
+        else if (
+            (humanChoice === "rock" && computerChoice === "scissors") ||
+            (humanChoice === "paper" && computerChoice === "rock") ||
+            (humanChoice === "scissors" && computerChoice === "paper")
+        ) {
             humanScore++;
-            return "You win! rock beats scissors!";
-        }
-        else if (humanChoice === "paper" && computerChoice === "scissors") {
-            computerScore++;
-            return "You lose... scissors beats paper!";
-        }
-        else if (humanChoice === "paper" && computerChoice === "rock") {
-            humanScore++;
-            return "You win! paper beats rock!";
-        }
-        else if (humanChoice === "scissors" && computerChoice == "rock") {
-            computerScore++;
-            return "You lose... rock beats scissors!";
-        }
-        else if (humanChoice === "scissors" && computerChoice == "paper") {
-            humanScore++;
-            return "You win! scissors beats rock!";
+            return `You win! ${humanChoice} beats ${computerChoice}!`;
         }
         else {
-            return "You must choose rock, paper, or scissors...";
+            return "You must choose rock, paper, or scissors";
         }
     }
 
