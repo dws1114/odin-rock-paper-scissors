4d2430a202c34721e11337a5957d64677128e8b6
dws1114 <161630399+dws1114@users.noreply.github.com>
2025-01-02 11:40:32 -0500

    Add play game function and place other functions inside game loop

--- javascript.js
+++ javascript.js
@@ -1,62 +1,69 @@
-const computerChoiceNum = Math.random();
+function playGame() {
 
-function getComputerChoice(num) {
-    if (num < 0.34) {
-        return "rock";
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === computerChoice) {
+            return `It's a tie! Both chose ${humanChoice}.`;
+        }
+        else if (humanChoice === "rock" && computerChoice === "paper") {
+            computerScore++;
+            return "You lose... paper beats rock!";
+        }
+        else if (humanChoice === "rock" && computerChoice === "scissors") {
+            humanScore++;
+            return "You win! rock beats scissors!";
+        }
+        else if (humanChoice === "paper" && computerChoice === "scissors") {
+            computerScore++;
+            return "You lose... scissors beats paper!";
+        }
+        else if (humanChoice === "paper" && computerChoice === "rock") {
+            humanScore++;
+            return "You win! paper beats rock!";
+        }
+        else if (humanChoice === "scissors" && computerChoice == "rock") {
+            computerScore++;
+            return "You lose... rock beats scissors!";
+        }
+        else if (humanChoice === "scissors" && computerChoice == "paper") {
+            humanScore++;
+            return "You win! scissors beats rock!";
+        }
+        else {
+            return "You must choose rock, paper, or scissors...";
+        }
     }
-    else if (num < 0.67) {
-        return "paper";
-    }
-    else {
-        return "scissors";
+
+    for (let i = 1; i <=5; i++) {
+        let computerChoiceNum = Math.random();
+
+        function getComputerChoice(num) {
+            if (num < 0.34) {
+                return "rock";
+            }
+            else if (num < 0.67) {
+                return "paper";
+            }
+            else {
+                return "scissors";
+            }
+        }
+    
+        function getHumanChoice() {
+            let humanChoice = prompt("What is your choice: Rock, Paper, or Scissors? ");
+            return humanChoice;
+        }
+    
+        let computerChoice = getComputerChoice(computerChoiceNum);
+        let humanChoice = getHumanChoice().toLowerCase();
+
+
+        let roundOutcome = playRound(humanChoice, computerChoice);
+        console.log(roundOutcome);
+        console.log(`Score - Human:${humanScore}, Computer:${computerScore}`);
     }
 }
 
-function getHumanChoice() {
-    const humanChoice = prompt("What is your choice: Rock, Paper, or Scissors? ");
-    return humanChoice;
-}
-
-const computerChoice = getComputerChoice(computerChoiceNum);
-const humanChoice = getHumanChoice().toLowerCase();
-
-let humanScore = 0;
-let computerScore = 0;
-
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-        return `It's a tie! Both chose ${humanChoice}.`;
-    }
-    else if (humanChoice === "rock" && computerChoice === "paper") {
-        computerScore++;
-        return "You lose... paper beats rock!";
-    }
-    else if (humanChoice === "rock" && computerChoice === "scissors") {
-        humanScore++;
-        return "You win! rock beats scissors!";
-    }
-    else if (humanChoice === "paper" && computerChoice === "scissors") {
-        computerScore++;
-        return "You lose... scissors beats paper!";
-    }
-    else if (humanChoice === "paper" && computerChoice === "rock") {
-        humanScore++;
-        return "You win! paper beats rock!";
-    }
-    else if (humanChoice === "scissors" && computerChoice == "rock") {
-        computerScore++;
-        return "You lose... rock beats scissors!";
-    }
-    else if (humanChoice === "scissors" && computerChoice == "paper") {
-        humanScore++;
-        return "You win! scissors beats rock!";
-    }
-    else {
-        return "You must choose rock, paper, or scissors...";
-    }
-}
-
-let roundOutcome = playRound(humanChoice, computerChoice);
-
-console.log(roundOutcome);
-console.log(`Score - Human:${humanScore}, Computer:${computerScore}`);
+console.log(playGame());
