05d34aa7a6ee89021324d53cc5f7e6fa1b449b43
dws1114 <161630399+dws1114@users.noreply.github.com>
2025-01-02 12:03:15 -0500

    Remove quoted out code

--- javascript.js
+++ javascript.js
@@ -3,39 +3,6 @@ function playGame() {
     let humanScore = 0;
     let computerScore = 0;
 
-    // function playRound(humanChoice, computerChoice) {
-    //     if (humanChoice === computerChoice) {
-    //         return `It's a tie! Both chose ${humanChoice}.`;
-    //     }
-    //     else if (humanChoice === "rock" && computerChoice === "paper") {
-    //         computerScore++;
-    //         return "You lose... paper beats rock!";
-    //     }
-    //     else if (humanChoice === "rock" && computerChoice === "scissors") {
-    //         humanScore++;
-    //         return "You win! rock beats scissors!";
-    //     }
-    //     else if (humanChoice === "paper" && computerChoice === "scissors") {
-    //         computerScore++;
-    //         return "You lose... scissors beats paper!";
-    //     }
-    //     else if (humanChoice === "paper" && computerChoice === "rock") {
-    //         humanScore++;
-    //         return "You win! paper beats rock!";
-    //     }
-    //     else if (humanChoice === "scissors" && computerChoice == "rock") {
-    //         computerScore++;
-    //         return "You lose... rock beats scissors!";
-    //     }
-    //     else if (humanChoice === "scissors" && computerChoice == "paper") {
-    //         humanScore++;
-    //         return "You win! scissors beats rock!";
-    //     }
-    //     else {
-    //         return "You must choose rock, paper, or scissors...";
-    //     }
-    // }
-
     function playRound(humanChoice, computerChoice) {
         if (humanChoice === computerChoice) {
             return `It's a tie! Both chose ${humanChoice}.`;
